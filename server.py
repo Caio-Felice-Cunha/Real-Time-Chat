57b5b53dfa31e849abd174191c52ad4a8ab7ef5c
Caio di Felice Cunha <caiofcunha@hotmail.com>
2024-11-19 10:04:15 -0800

    Defining the server (socket). Creating the Broadcast [still not finished]. Doing some tests with the broadcast

---
+++ server.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+rooms = {}
+
+def broadcast(room, message):
+    for i in rooms[room]:
+        if isinstance(message, str):
+            message = message.encode()
+        #i.send(message)
+        print(i)
+
+while True:
+    client, addr = server.accept()
+    
+    client.send(b'SALA')
+    room = client.recv(1024).decode()
+    name = client.recv(1024).decode()
+    
+    if room not in rooms.keys():
+        rooms[room] = []
+
+    rooms[room].append(client)
+
+    print(f'{name} has connected in the room {room}! INFO {addr}')
+
+    broadcast(room, f'{name} just joined the room! \n')
